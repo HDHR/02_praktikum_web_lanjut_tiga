16cf8c45e58641cf3a53256f9bbadbc12a043a65
HDHR <23426661+HDHR@users.noreply.github.com>
2021-02-24 11:53:13 +0700

    Praktikum 3 - Multi Routing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ProgramController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,50 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// 1. - Halaman Home
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('https://www.educastudio.com/');
 });
+
+// 2. Halaman Products
+Route::prefix('/category')->group(function () {
+    Route::get('/marbel-edu-games', function () {
+        return redirect('https://www.educastudio.com/category/marbel-edu-games');
+    });
+
+    Route::get('/riri-story-books', function () {
+        return redirect('https://www.educastudio.com/category/riri-story-books');
+    });
+
+    Route::get('/kolak-kids-songs', function () {
+        return redirect('https://www.educastudio.com/category/kolak-kids-songs');
+    });
+});
+
+// 3. Halaman News
+Route::get('news', function () {
+    return redirect('https://www.educastudio.com/news');
+});
+
+Route::get('news/{title}', function ($title) {
+    return redirect('https://www.educastudio.com/news/' . $title);
+});
+
+// 4. Halaman Program
+Route::prefix('/program')->group(function () {
+    Route::get('/karir', [ProgramController::class, 'karir']);
+
+    Route::get('/magang', [ProgramController::class, 'magang']);
+
+    Route::get('/kunjungan-industri', [ProgramController::class, 'kunjunganIndustri']);
+});
+
+// 5 - Halaman About Us
+Route::get('/about-us', function () {
+    return redirect('https://www.educastudio.com/about-us');
+});
+
+// 6 - Halaman Contact Us
+Route::resource('/contact-us', ContactController::class, [
+    'only' => ['index']
+]);
